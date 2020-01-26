08bc93e604ff56e835a5b76498baea728267adf8
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2020-01-26 17:55:19 +0000

    Automatically backed up by Learn

---
+++ learning_ifelse.rb
@@ -0,0 +1,9 @@
+chance_of_rain = 0.2 
+if chance_of_rain <=0.25
+  puts "pack a sun shelter!"
+elseif (chance_of_rain > 0.25 && chance_of_rain <0.75)
+  puts "Pack an umberlla!"
+else
+  puts "stay at home and read Hegel"
+end
+ruby learning_ifelse.rb 
